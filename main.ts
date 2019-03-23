33ef168883ddf1d850425cd3de82fd721cf2017b
cobit-kr <conner.jeong@gmail.com>
2019-03-23 17:36:58 +0900

    direction 4

--- main.ts
+++ main.ts
@@ -111,4 +111,113 @@ namespace cobit_draw {
         }
 
     }
+
+     /**
+     * Turn left 
+     *  * @param angle angle of motor
+     */
+    //% angle.min=0 angle.max=90
+    //% weight=90
+    //% blockId="cobit_turnLeft" block="왼쪽으로 %angle|% 도만큼 돌기"
+    export function goTurnLeft(angle: number): void {
+
+        if (angle > 90) {
+            angle = 90
+        }
+        for (let i = 0; i < angle; i++) {
+            pins.digitalWritePin(DigitalPin.P16, 1)
+            pins.digitalWritePin(DigitalPin.P15, 0)
+            pins.digitalWritePin(DigitalPin.P14, 0)
+            pins.digitalWritePin(DigitalPin.P13, 1)
+            pins.digitalWritePin(DigitalPin.P10, 1)
+            pins.digitalWritePin(DigitalPin.P9, 0)
+            pins.digitalWritePin(DigitalPin.P7, 0)
+            pins.digitalWritePin(DigitalPin.P6, 1)
+            basic.pause(1)
+            pins.digitalWritePin(DigitalPin.P16, 0)
+            pins.digitalWritePin(DigitalPin.P15, 0)
+            pins.digitalWritePin(DigitalPin.P14, 1)
+            pins.digitalWritePin(DigitalPin.P13, 1)
+            pins.digitalWritePin(DigitalPin.P10, 0)
+            pins.digitalWritePin(DigitalPin.P9, 0)
+            pins.digitalWritePin(DigitalPin.P7, 1)
+            pins.digitalWritePin(DigitalPin.P6, 1)
+            basic.pause(1)
+            pins.digitalWritePin(DigitalPin.P16, 0)
+            pins.digitalWritePin(DigitalPin.P15, 1)
+            pins.digitalWritePin(DigitalPin.P14, 1)
+            pins.digitalWritePin(DigitalPin.P13, 0)
+            pins.digitalWritePin(DigitalPin.P10, 0)
+            pins.digitalWritePin(DigitalPin.P9, 1)
+            pins.digitalWritePin(DigitalPin.P7, 1)
+            pins.digitalWritePin(DigitalPin.P6, 0)
+            basic.pause(1)
+            pins.digitalWritePin(DigitalPin.P16, 1)
+            pins.digitalWritePin(DigitalPin.P15, 1)
+            pins.digitalWritePin(DigitalPin.P14, 0)
+            pins.digitalWritePin(DigitalPin.P13, 0)
+            pins.digitalWritePin(DigitalPin.P10, 1)
+            pins.digitalWritePin(DigitalPin.P9, 1)
+            pins.digitalWritePin(DigitalPin.P7, 0)
+            pins.digitalWritePin(DigitalPin.P6, 0)
+            basic.pause(1)
+
+        }
+
+    }
+
+
+     /**
+     * Turn right 
+     *  * @param angle angle of motor
+     */
+    //% angle.min=0 angle.max=90
+    //% weight=90
+    //% blockId="cobit_turnRight" block="오른쪽으로 %angle|% 도만큼 돌기"
+    export function goTurnRight(angle: number): void {
+
+        if (angle > 90) {
+            angle = 90
+        }
+        for (let i = 0; i < angle; i++) {
+            pins.digitalWritePin(DigitalPin.P13, 1)
+            pins.digitalWritePin(DigitalPin.P14, 0)
+            pins.digitalWritePin(DigitalPin.P15, 0)
+            pins.digitalWritePin(DigitalPin.P16, 1)
+            pins.digitalWritePin(DigitalPin.P10, 1)
+            pins.digitalWritePin(DigitalPin.P9, 0)
+            pins.digitalWritePin(DigitalPin.P7, 0)
+            pins.digitalWritePin(DigitalPin.P6, 1)
+            basic.pause(1)
+            pins.digitalWritePin(DigitalPin.P13, 0)
+            pins.digitalWritePin(DigitalPin.P14, 0)
+            pins.digitalWritePin(DigitalPin.P15, 1)
+            pins.digitalWritePin(DigitalPin.P16, 1)
+            pins.digitalWritePin(DigitalPin.P10, 0)
+            pins.digitalWritePin(DigitalPin.P9, 0)
+            pins.digitalWritePin(DigitalPin.P7, 1)
+            pins.digitalWritePin(DigitalPin.P6, 1)
+            basic.pause(1)
+            pins.digitalWritePin(DigitalPin.P13, 0)
+            pins.digitalWritePin(DigitalPin.P14, 1)
+            pins.digitalWritePin(DigitalPin.P15, 1)
+            pins.digitalWritePin(DigitalPin.P16, 0)
+            pins.digitalWritePin(DigitalPin.P10, 0)
+            pins.digitalWritePin(DigitalPin.P9, 1)
+            pins.digitalWritePin(DigitalPin.P7, 1)
+            pins.digitalWritePin(DigitalPin.P6, 0)
+            basic.pause(1)
+            pins.digitalWritePin(DigitalPin.P13, 1)
+            pins.digitalWritePin(DigitalPin.P14, 1)
+            pins.digitalWritePin(DigitalPin.P15, 0)
+            pins.digitalWritePin(DigitalPin.P16, 0)
+            pins.digitalWritePin(DigitalPin.P10, 1)
+            pins.digitalWritePin(DigitalPin.P9, 1)
+            pins.digitalWritePin(DigitalPin.P7, 0)
+            pins.digitalWritePin(DigitalPin.P6, 0)
+            basic.pause(1)
+
+        }
+
+    }
 }
